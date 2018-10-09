232c1a7c8348332b8eadcad1d8f69b8cca92c3ff
Dawid Janik <daw_janik@protonmail.com>
2018-10-09 18:56:35 +0200

    Impl new app context test.

--- src/main/java/pl/dsyou/minesweeper/Application.java
+++ src/main/java/pl/dsyou/minesweeper/Application.java
@@ -1,15 +1,10 @@
 package pl.dsyou.minesweeper;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-
-import java.util.Arrays;
 
 /**
  * @author Dawid Janik
@@ -17,39 +12,23 @@ import java.util.Arrays;
  */
 @Slf4j
 @EntityScan()
-@ComponentScan()
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> getBeanNames(ctx);
-    }
-
-    private void getBeanNames(ApplicationContext ctx) {
-        log.info("Let's inspect the beans provided by Spring Boot:");
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            log.info(beanName);
-        }
-    }
-
-            //        Game game = new Game();
-        //
-        //        if (args.length == 1) {
-        //            String field = args[0]; // 1 Program arguments taken form user, represent field
-        //            game.startGame(field);
-        //        } else {
-        //            throw new MineFiledFormattedException();
-        //        }
-        //
-        //        field.showField();
-        //        System.out.println(field.getHintField());
-        ////      a.showHint(); //todo user should have option in game to see hints
+    //        Game game = new Game();
+    //
+    //        if (args.length == 1) {
+    //            String field = args[0]; // 1 Program arguments taken form user, represent field
+    //            game.startGame(field);
+    //        } else {
+    //            throw new MineFiledFormattedException();
+    //        }
+    //
+    //        field.showField();
+    //        System.out.println(field.getHintField());
+    ////      a.showHint(); //todo user should have option in game to see hints
 
 }
---
+++ src/test/java/pl/dsyou/minesweeper/ApplicationContextTest.java
@@ -0,0 +1,20 @@
+package pl.dsyou.minesweeper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Dawid
+ * @since 09.10.2018
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ApplicationContextTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
